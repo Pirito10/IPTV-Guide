bd8cad6c310533b76dc61ee98d312b48d5b1715a
Pirito10 <aaronriveiro@gmail.com>
2025-03-10 23:03:55 +0100

    migrate to react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Guía de TV</h1>
+      <p>Bienvenido a la interfaz de la guía de TV en React con Vite.</p>
+    </div>
+  )
+}
+
+export default App
