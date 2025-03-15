6e96ecb022b855bd7bbe67a47f9473d6d10605fb
Pirito10 <aaronriveiro@gmail.com>
2025-03-15 01:08:42 +0100

    increase day width

--- frontend/src/components/TVGuide.jsx
+++ frontend/src/components/TVGuide.jsx
@@ -18,6 +18,7 @@ const TVGuide = () => {
         channels: formattedChannels,
         width: 1850,
         height: 900,
+        dayWidth: 10000,
         startDate: new Date().toISOString().split("T")[0] + "T00:00:00", // Fecha actual
     });
 
