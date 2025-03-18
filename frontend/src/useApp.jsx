dd767edc7b472d6fae2472fe81d73f282ccd5f7e
Pirito10 <aaronriveiro@gmail.com>
2025-03-18 02:28:22 +0100

    create useApp.jsx

---
+++ frontend/src/useApp.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, useMemo, useCallback } from 'react'
+import { useEpg } from 'planby'
+import { fetchData } from './helpers'
+
+export const useApp = () => {
+    const [channels, setChannels] = useState([]) // Estado para los canales
+    const [epg, setEpg] = useState([]) // Estado para la guía EPG
+    const [isLoading, setIsLoading] = useState(true) // Estado de carga
+    const [hasScrolled, setHasScrolled] = useState(false) // Estado para controlar el scroll a la hora actual
+
+    // Variables para los datos de los canales y la guía EPG en formato de Planby
+    const channelsData = useMemo(() => channels, [channels])
+    const epgData = useMemo(() => epg, [epg])
+
+    // Configuración de Planby
+    const epgProps = useEpg({
+        channels: channelsData,
+        epg: epgData,
+        width: 1850,
+        height: 900,
+        dayWidth: 10000,
+        startDate: new Date().toISOString().split("T")[0] + "T00:00:00", // Fecha actual
+    });
+
+    // Función para cargar los datos de los canales y la guía EPG
+    const handleFetchResources = useCallback(async () => {
+        setIsLoading(true);
+        const { channels, epg } = await fetchData();
+        setChannels(channels);
+        setEpg(epg);
+        setIsLoading(false);
+    }, []);
+
+    // Ejecutamos la carga de datos al montar el componente
+    useEffect(() => {
+        handleFetchResources();
+    }, [handleFetchResources]);
+
+    // Ejecutamos un scroll a la hora actual
+    useEffect(() => {
+        if (!hasScrolled && epgProps.onScrollToNow) {
+            epgProps.onScrollToNow();
+            setHasScrolled(true)
+        }
+    }, [hasScrolled, epgProps]);
+
+    return { epgProps, isLoading };
+}
