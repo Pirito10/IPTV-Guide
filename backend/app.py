727b64c745ec02d83dee09a6188b2ce310a890f4
Pirito10 <aaronriveiro@gmail.com>
2025-03-21 21:03:59 +0100

    move routes to routes.py

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from functools import partial
-import data
 import updater
+import routes
 
 # Servidor Flask
 app = Flask(__name__)
 CORS(app)
 
+app.register_blueprint(routes.routes)
+
 # Planificador de tareas
 scheduler = BackgroundScheduler()
-
-# Ruta a la API para obtener la guía EPG
-@app.route("/api/epg", methods=["GET"])
-def epg():
-    # Devolvemos la guía almacenada en caché
-     return jsonify(data.cached_epg_data)
-
-# Ruta a la API para obtener los canales
-@app.route("/api/channels", methods=["GET"])
-def channels():
-    # Verificamos si se ha actualizado la lista M3U en el último minuto
-    if data.last_m3u_update and (datetime.now() - data.last_m3u_update).seconds < 60:
-        print("La lista M3U se actualizó hace menos de 1 minuto, usando caché...")
-    else:
-        # Si ha pasado más de un minuto, actualizamos la lista M3U
-        updater.update_m3u()
-
-    # Devolvemos la lista de canales almacenada en caché
-    return jsonify(data.cached_m3u_data)
 
 # Programamos la actualización de la guía EPG
 scheduler.add_job(partial(updater.update_epg, scheduler), "cron", hour="10,14,18,22", minute=0)
---
+++ backend/routes.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from flask import jsonify, Blueprint
+import data
+import updater
+
+routes = Blueprint("routes", __name__)
+
+# Ruta a la API para obtener la guía EPG
+@routes.route("/api/epg", methods=["GET"])
+def epg():
+    # Devolvemos la guía almacenada en caché
+     return jsonify(data.cached_epg_data)
+
+# Ruta a la API para obtener los canales
+@routes.route("/api/channels", methods=["GET"])
+def channels():
+    # Verificamos si se ha actualizado la lista M3U en el último minuto
+    if data.last_m3u_update and (datetime.now() - data.last_m3u_update).seconds < 60:
+        print("La lista M3U se actualizó hace menos de 1 minuto, usando caché...")
+    else:
+        # Si ha pasado más de un minuto, actualizamos la lista M3U
+        updater.update_m3u()
+
+    # Devolvemos la lista de canales almacenada en caché
+    return jsonify(data.cached_m3u_data)
