96cbae4589f2a1b4c524d7c7d37427c07c8fc960
Pirito10 <aaronriveiro@gmail.com>
2025-03-21 19:46:17 +0100

    update comment

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ M3U_URL = "http://127.0.0.1:43110/1H3KoazXt2gCJgeD8673eFvQYXG7cbRddU/lista-ace.m
 # Variables para almacenamiento en caché
 cached_epg_data = {}
 cached_m3u_data = []
-# Contandor de intentos fallidos
+# Contandor de intentos fallidos de descarga de la guía EPG
 epg_retry_count = 0
 # Fecha de la última actualización de la lista M3U
 last_m3u_update = None
