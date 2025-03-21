7b5ca848b4d4425b173a309fdd184a0b804f20e0
Pirito10 <aaronriveiro@gmail.com>
2025-03-21 21:24:08 +0100

    update comments

--- backend/cache.py
+++ backend/cache.py
@@ -1,4 +1,4 @@
-# Variables para almacenamiento en caché
+# Variables para almacenar en caché la lista M3U y la guía EPG
 cached_epg_data = {}
 cached_m3u_data = []
 
--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
-# Enlaces a los archivos
-EPG_URL = "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiatv.xml"
+# Enlaces a los archivos de la lista M3U y la guía EPG
 M3U_URL = "http://127.0.0.1:43110/1H3KoazXt2gCJgeD8673eFvQYXG7cbRddU/lista-ace.m3u"
+EPG_URL = "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiatv.xml"
 
 # ID por defecto para los canales con ID desconocido
 DEFAULT_ID = "unknown"
