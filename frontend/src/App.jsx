870615f10e6d8c510d5b59702774775700db86fa
Pirito10 <aaronriveiro@gmail.com>
2025-03-23 01:26:34 +0100

    render channel modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Epg, Layout } from 'planby'
 import { useApp } from './useApp'
-import { Timeline, ChannelItem, ProgramItem } from './components'
+import { Timeline, ChannelItem, ChannelModal, ProgramItem } from './components'
 
 const App = () => {
     // Obtenemos los datos y propiedades de la guía de programación
     const { epgProps, isLoading } = useApp()
+
+    const [selectedChannel, setSelectedChannel] = useState(null) // Estado para el canal seleccionado
 
     // Renderizamos el HTML de la guía de programación
     return (
@@ -14,10 +16,11 @@ const App = () => {
                 <Layout
                     {...epgProps.getLayoutProps()}
                     renderTimeline={(props) => <Timeline {...props} />}
-                    renderChannel={(props) => <ChannelItem key={props.channel.uuid} channel={props.channel} />}
+                    renderChannel={(props) => <ChannelItem key={props.channel.uuid} channel={props.channel} onClick={() => setSelectedChannel(props.channel)} />}
                     renderProgram={(props) => <ProgramItem key={props.program.data.id} {...props} />}
                 />
             </Epg>
+            {selectedChannel && <ChannelModal channel={selectedChannel} onClose={() => setSelectedChannel(null)} />}
         </div>
     )
 }
