0d2ca4194eaef8d144b171c4025faf38cc2f01fa
Pirito10 <aaronriveiro@gmail.com>
2025-03-28 02:13:56 +0100

    use fallback_logo if the provided returns an error

--- frontend/src/components/ChannelItem.jsx
+++ frontend/src/components/ChannelItem.jsx
@@ -1,4 +1,5 @@
 import { ChannelBox, ChannelLogo } from 'planby';
+import { FALLBACK_LOGO } from '../helpers/constants';
 
 export const ChannelItem = ({ channel, onClick }) => {
     const { position, logo } = channel;
@@ -10,9 +11,11 @@ export const ChannelItem = ({ channel, onClick }) => {
         >
             <ChannelLogo
                 src={logo}
+                // Ponemos el logo de respaldo si hubo un error al cargar la imagen
+                onError={e => e.currentTarget.src = FALLBACK_LOGO}
                 // Añadimos un efecto de zoom al pasar el ratón
-                onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.3)')}
-                onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
+                onMouseOver={e => e.currentTarget.style.transform = 'scale(1.3)'}
+                onMouseOut={e => e.currentTarget.style.transform = 'scale(1)'}
                 style={{
                     transition: 'transform 0.2s',
                     cursor: 'pointer'
