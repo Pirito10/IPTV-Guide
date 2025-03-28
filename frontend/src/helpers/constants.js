29c774dc6936a004dcdf47ffb697ea7116f2ce7c
Pirito10 <aaronriveiro@gmail.com>
2025-03-28 02:20:14 +0100

    add comments

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,5 +1,7 @@
+// URLs a los endpoints del backend
 export const CHANNELS_URL = "http://127.0.0.1:5000/api/channels"
 export const EPG_URL = "http://127.0.0.1:5000/api/epg"
 
+// Valores por defecto para los parámetros de los canales
 export const FALLBACK_LOGO = "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
 export const DEFAULT_GROUP = "OTROS"
