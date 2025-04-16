b8f4d5a39873e0550023ebf061988bbd383cdb6c
Pirito10 <aaronriveiro@gmail.com>
2025-04-17 01:17:40 +0200

    refactor app.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from services.updater import update_m3u, update_epg
 from config import config
 from services.logger import logger
 
+logger.info("Starting IPTV-Guide server...")
+
 # Servidor Flask
 app = Flask(__name__)
 app.register_blueprint(routes)
@@ -14,20 +16,18 @@ CORS(app)
 # Planificador de tareas
 scheduler = BackgroundScheduler()
 
+# Actualizamos la guía EPG y la lista M3U
+logger.info("Running initial M3U list and EPG update...")
+update_m3u(first_run=True)
+update_epg(scheduler, first_run=True)
+
+# Programamos las actualizaciones de la guía EPG
+scheduler.add_job(lambda: (update_m3u(force=True), update_epg(scheduler)), "cron", hour=config.EPG_SCHEDULER_HOURS, minute=0)
+scheduler.start()
+logger.info(f"EPG update scheduled at hours: {config.EPG_SCHEDULER_HOURS}")
+
 if __name__ == "__main__":
     try:
-        logger.info("Starting IPTV-Guide server...")
-
-        # Actualizamos la guía EPG y la lista M3U
-        logger.info("Running initial M3U list and EPG update...")
-        update_m3u(first_run=True)
-        update_epg(scheduler, first_run=True)
-
-        # Programamos las actualizaciones de la guía EPG
-        scheduler.add_job(lambda: (update_m3u(force=True), update_epg(scheduler)), "cron", hour=config.EPG_SCHEDULER_HOURS, minute=0)
-        scheduler.start()
-        logger.info(f"EPG update scheduled at hours: {config.EPG_SCHEDULER_HOURS}")
-
         # Iniciamos el servidor Flask
         logger.info(f"Server running at http://{config.SERVER_IP}:5000")
         app.run(config.SERVER_IP)
