13ab9704dfc8fe764d3dc6c7dbd401316f754517
Pirito10 <aaronriveiro@gmail.com>
2025-04-19 23:15:40 +0200

    add mutual exclusion to channels route

--- backend/routes.py
+++ backend/routes.py
@@ -1,3 +1,4 @@
+import threading
 from flask import jsonify, Blueprint
 from services.logger import logger
 from services.updater import update_m3u
@@ -5,16 +6,19 @@ from config import cache
 
 routes = Blueprint("routes", __name__)
 
+m3u_lock = threading.Lock()
+
 # Ruta a la API para obtener los canales
 @routes.route("/api/channels")
 def channels():
     logger.info("Request to /api/channels received")
 
-    # Intentamos actualizar la lista M3U
-    try:
-        update_m3u()
-    except Exception as e:
-        logger.error(f"Unexpected error while updating M3U list: {e}")
+    # Intentamos actualizar la lista M3U en exclusión mutua
+    with m3u_lock:
+        try:
+            update_m3u()
+        except Exception as e:
+            logger.error(f"Unexpected error while updating M3U list: {e}")
 
     # Si no hay datos de canales, devolvemos un error
     if not cache.cached_m3u_data:
