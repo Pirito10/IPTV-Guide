c650fce956dc3824ac4f8c46e683a4ae521f7bad
Pirito10 <aaronriveiro@gmail.com>
2025-04-20 00:49:44 +0200

    add toolbar to interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import { Epg, Layout } from 'planby'
 import { useApp } from '@/useApp'
-import { Timeline, ChannelItem, ChannelModal, ProgramItem, ProgramModal } from '@components'
+import { Toolbar, Timeline, ChannelItem, ChannelModal, ProgramItem, ProgramModal } from '@components'
 import '@styles/global.css'
 
 const App = () => {
     // Obtenemos los datos y propiedades de la guía de programación
     const { epgProps, isLoading } = useApp()
+
+    // Obtenemos la altura de la barra de herramientas
+    const toolbarHeight = getComputedStyle(document.documentElement).getPropertyValue('--toolbar-height')
 
     const [selectedChannel, setSelectedChannel] = useState(null) // Estado para el canal seleccionado
     const [selectedProgram, setSelectedProgram] = useState(null) // Estado para el programa seleccionado
@@ -14,14 +17,17 @@ const App = () => {
     // Renderizamos el HTML de la guía de programación
     return (
         <div id="epg-root">
-            <Epg isLoading={isLoading} {...epgProps.getEpgProps()} style={{ padding: 0 }} >
-                <Layout
-                    {...epgProps.getLayoutProps()}
-                    renderTimeline={(props) => <Timeline {...props} />}
-                    renderChannel={(props) => <ChannelItem key={props.channel.uuid} channel={props.channel} onClick={() => setSelectedChannel(props.channel)} />}
-                    renderProgram={(props) => <ProgramItem key={props.program.data.id} onClick={() => setSelectedProgram(props.program.data)} {...props} />}
-                />
-            </Epg>
+            <Toolbar />
+            <div style={{ height: `calc(100% - ${toolbarHeight})` }}>
+                <Epg isLoading={isLoading} {...epgProps.getEpgProps()} style={{ padding: 0 }} >
+                    <Layout
+                        {...epgProps.getLayoutProps()}
+                        renderTimeline={(props) => <Timeline {...props} />}
+                        renderChannel={(props) => <ChannelItem key={props.channel.uuid} channel={props.channel} onClick={() => setSelectedChannel(props.channel)} />}
+                        renderProgram={(props) => <ProgramItem key={props.program.data.id} onClick={() => setSelectedProgram(props.program.data)} {...props} />}
+                    />
+                </Epg>
+            </div>
             {selectedChannel && <ChannelModal channel={selectedChannel} onClose={() => setSelectedChannel(null)} />}
             {selectedProgram &&
                 <ProgramModal
