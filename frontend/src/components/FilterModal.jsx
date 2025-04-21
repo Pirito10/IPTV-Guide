b89abe55c7421cc9b8cd2d44cacb68d68520b9cd
Pirito10 <aaronriveiro@gmail.com>
2025-04-22 00:50:20 +0200

    use generic modal on FilterModal

--- frontend/src/components/FilterModal.jsx
+++ frontend/src/components/FilterModal.jsx
@@ -1,21 +1,29 @@
-import { useEffect } from 'react'
-import { FaTimes } from 'react-icons/fa'
+import { Modal } from '@components/Modal'
 import '@styles/Modal.css'
 import '@styles/FilterModal.css'
 
 // Componente para mostrar un modal con los grupos para filtrar
 export const FilterModal = ({ groups, selectedGroups, onChange, onClose }) => {
-    // Listener para la tecla ESC
-    useEffect(() => {
-        // Creamos una función para manejar el evento "keydown"
-        const handleKeyDown = (e) => {
-            if (e.key === 'Escape') onClose()
-        }
-        // Añadimos el listener al documento
-        window.addEventListener('keydown', handleKeyDown)
-        // Eliminamos el listener al cerrar el modal
-        return () => window.removeEventListener('keydown', handleKeyDown)
-    })
+    // Creamos la cabecera del modal
+    const header = (
+        <h2 className="modal-title">Grupos de canales</h2>
+    )
+
+    // Creamos el cuerpo del modal
+    const body = (
+        <div className="groups-container">
+            {groups.map((group) => (
+                <label key={group} className="group">
+                    <input
+                        type="checkbox"
+                        checked={selectedGroups.includes(group)}
+                        onChange={() => { toggleGroup(group) }}
+                    />
+                    {group}
+                </label>
+            ))}
+        </div>
+    )
 
     // Función para cambiar la selección de un grupo
     const toggleGroup = (group) => {
@@ -26,26 +34,6 @@ export const FilterModal = ({ groups, selectedGroups, onChange, onClose }) => {
     }
 
     return (
-        <div className="modal-backdrop" onClick={onClose}>
-            <div className="modal" onClick={(e) => { e.stopPropagation() }}>
-                <div className="modal-header">
-                    <h2 className="modal-title">Grupos de canales</h2>
-                    <button className="modal-close" onClick={onClose}><FaTimes /></button>
-                </div>
-
-                <div className="modal-body groups-container">
-                    {groups.map((group) => (
-                        <label key={group} className="group">
-                            <input
-                                type="checkbox"
-                                checked={selectedGroups.includes(group)}
-                                onChange={() => { toggleGroup(group) }}
-                            />
-                            {group}
-                        </label>
-                    ))}
-                </div>
-            </div>
-        </div>
+        <Modal header={header} body={body} onClose={onClose} />
     )
 }
