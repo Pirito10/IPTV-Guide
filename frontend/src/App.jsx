9db747f72e1fbc1f7fdeaccd61149a0b730116fd
Pirito10 <aaronriveiro@gmail.com>
2025-04-21 02:08:05 +0200

    add handler for group change in order to show an animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,13 @@ const App = () => {
     const [selectedChannel, setSelectedChannel] = useState(null) // Estado para el canal seleccionado
     const [selectedProgram, setSelectedProgram] = useState(null) // Estado para el programa seleccionado
     const [selectedGroups, setSelectedGroups] = useState(null) // Estado para el grupo seleccionado
+    const [isFiltering, setIsFiltering] = useState(false) // Estado de carga por filtrado
 
     // Obtenemos los datos y propiedades de la guía de programación
     const { epgProps, isLoading, groups } = useApp(selectedGroups)
+
+    // Obtenemos la altura de la barra de herramientas
+    const toolbarHeight = getComputedStyle(document.documentElement).getPropertyValue('--toolbar-height')
 
     // Establecemos todos los grupos como seleccionados por defecto
     useEffect(() => {
@@ -19,15 +23,29 @@ const App = () => {
         }
     }, [groups])
 
-    // Obtenemos la altura de la barra de herramientas
-    const toolbarHeight = getComputedStyle(document.documentElement).getPropertyValue('--toolbar-height')
+    // Función para manejar el cambio de grupo seleccionado
+    const handleGroupChange = (newGroups) => {
+        // Marcamos el inicio del filtrado para mostrar una animación
+        setIsFiltering(true)
+
+        // Reservamos un frame para mostrar la animación
+        requestAnimationFrame(() => {
+            // Actualizamos el estado de los grupos seleccionados
+            setSelectedGroups(newGroups)
+
+            // Marcamos el fin del filtrado después de un tiempo
+            setTimeout(() => {
+                setIsFiltering(false)
+            }, 300)
+        })
+    }
 
     // Renderizamos el HTML de la guía de programación
     return (
         <div id="epg-root">
-            <Toolbar groups={groups} selectedGroups={selectedGroups} onGroupChange={setSelectedGroups} />
+            <Toolbar groups={groups} selectedGroups={selectedGroups} onGroupChange={handleGroupChange} />
             <div style={{ height: `calc(100% - ${toolbarHeight})` }}>
-                <Epg isLoading={isLoading} {...epgProps.getEpgProps()} style={{ padding: 0 }} >
+                <Epg isLoading={isLoading || isFiltering} {...epgProps.getEpgProps()} style={{ padding: 0 }} >
                     <Layout
                         {...epgProps.getLayoutProps()}
                         renderTimeline={(props) => <Timeline {...props} />}
