5f677072121b13013ad6aa670eb783ed15bb5f6e
Pirito10 <aaronriveiro@gmail.com>
2025-04-21 02:15:30 +0200

    add filter icon to button

--- frontend/src/components/Toolbar.jsx
+++ frontend/src/components/Toolbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaFilter } from 'react-icons/fa'
 import { FilterModal } from '@components'
 import '@styles/Toolbar.css'
 
@@ -8,7 +9,10 @@ export const Toolbar = ({ groups, selectedGroups, onGroupChange }) => {
 
     return (
         <div className="toolbar">
-            <button className="toolbar-button" onClick={() => setShowFilterModal(true)}>Filtrar</button>
+            <button className="toolbar-button" onClick={() => setShowFilterModal(true)}>
+                <FaFilter className="toolbar-button-icon" />
+                Filtrar
+            </button>
             <input className="toolbar-search" type="text" placeholder="Buscar canales..." />
             <button className="toolbar-button">Info</button>
 
