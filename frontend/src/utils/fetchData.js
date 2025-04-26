927e8dca132aed346eddb67c8a8cb1a934c57014
Pirito10 <aaronriveiro@gmail.com>
2025-04-26 20:01:37 +0200

    improve fetchData to still return channel data if only epg fetch failed

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -5,24 +5,27 @@ import { CHANNELS_URL, EPG_URL } from '@utils/constants'
 export const fetchData = async () => {
     try {
         // Hacemos las peticiones a la API
-        const [channelsResponse, epgResponse] = await Promise.all([
-            fetch(CHANNELS_URL),
-            fetch(EPG_URL)
-        ])
+        const channelsResponse = await fetch(CHANNELS_URL)
+        const epgResponse = await fetch(EPG_URL)
 
+        // Comprobamos si la respuesta para los canales es correcta
         if (!channelsResponse.ok) {
+            // Si no es correcta, lanzamos un error y no se devuelven los datos
             throw new Error(`\nNo se han podido descargar los canales: ${channelsResponse.status} - ${channelsResponse.statusText}`)
         }
-
-        if (!epgResponse.ok) {
-            throw new Error(`\nNo se ha podido descargar la EPG: ${epgResponse.status} - ${epgResponse.statusText}`)
+        // Comprobamos si la respuesta para la EPG es correcta
+        try {
+            if (!epgResponse.ok) {
+                // Si no es correcta, lanzamos un error y solo se devuelven los datos de los canales
+                throw new Error(`\nNo se ha podido descargar la EPG: ${epgResponse.status} - ${epgResponse.statusText}`)
+            }
+        } catch (error) {
+            console.error("Error obteniendo los datos del servidor", error.message)
         }
 
         // Convertimos los datos a formato JSON
-        const [channelsData, epgData] = await Promise.all([
-            channelsResponse.json(),
-            epgResponse.json()
-        ])
+        const channelsData = await channelsResponse.json()
+        const epgData = await epgResponse.json()
 
         // Convertimos los datos al formato de Planby
         const channels = formatChannels(channelsData)
