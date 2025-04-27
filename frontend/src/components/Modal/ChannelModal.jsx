29b87127fea0471b4e13a4becd193f89c3a9aa69
Pirito10 <aaronriveiro@gmail.com>
2025-04-27 02:48:15 +0200

    refactor ChannelModal

--- frontend/src/components/Modal/ChannelModal.jsx
+++ frontend/src/components/Modal/ChannelModal.jsx
@@ -5,45 +5,21 @@ import '@styles/Modal/ChannelModal.css'
 
 // Componente para mostrar un modal con los streams de un canal
 export const ChannelModal = ({ channel, onClose }) => {
-    // Creamos la cabecera del modal
-    const header = (
-        <>
-            <img
-                className="modal-logo"
-                src={channel.logo}
-                onError={e => e.currentTarget.src = FALLBACK_LOGO}
-            />
-            <h2 className="modal-title">{channel.uuid}</h2>
-        </>
-    )
+    // Función para poner el logo de respaldo
+    const handleImageError = e => {
+        e.currentTarget.src = FALLBACK_LOGO
+    }
 
-    // Creamos el cuerpo del modal
-    const body = (
-        <>
-            {channel.streams?.map((stream, index) => (
-                <div key={index} className="channel-stream">
-                    <div className="channel-stream-name">{stream.name}</div>
-                    <button
-                        className="channel-stream-button channel-stream-copy-button"
-                        onClick={() => {
-                            navigator.clipboard.writeText(stream.url)
-                            showCopiedToast()
-                        }}
-                    >
-                        <FaRegCopy className="channel-stream-button-icon" />
-                        Copiar ID
-                    </button>
-                    <button
-                        className="channel-stream-button channel-stream-play-button"
-                        onClick={() => window.open(`acestream://${stream.url}`)}
-                    >
-                        <FaPlay className="channel-stream-button-icon" />
-                        Reproducir
-                    </button>
-                </div>
-            ))}
-        </>
-    )
+    // Función para copiar un ID de un stream
+    const handleCopy = url => {
+        navigator.clipboard.writeText(url)
+        showCopiedToast()
+    }
+
+    // Función para reproducir un stream
+    const handlePlay = url => {
+        window.open(`acestream://${url}`)
+    }
 
     // Función para mostrar el toast de ID copiado
     const showCopiedToast = () => {
@@ -63,6 +39,43 @@ export const ChannelModal = ({ channel, onClose }) => {
             toast.remove()
         }, 2000)
     }
+
+    // Creamos la cabecera del modal, con el logo y nombre del canal
+    const header = (
+        <>
+            <img
+                className="modal-logo"
+                src={channel.logo}
+                onError={handleImageError}
+            />
+            <h2 className="modal-title">{channel.uuid}</h2>
+        </>
+    )
+
+    // Creamos el cuerpo del modal, con una entrada para cada stream con el nombre, botón para copiar ID y botón para reproducir
+    const body = (
+        <>
+            {channel.streams?.map((stream, index) => (
+                <div key={index} className="channel-stream">
+                    <div className="channel-stream-name">{stream.name}</div>
+                    <button
+                        className="channel-stream-button channel-stream-copy-button"
+                        onClick={() => handleCopy(stream.url)}
+                    >
+                        <FaRegCopy className="channel-stream-button-icon" />
+                        Copiar ID
+                    </button>
+                    <button
+                        className="channel-stream-button channel-stream-play-button"
+                        onClick={() => handlePlay(stream.url)}
+                    >
+                        <FaPlay className="channel-stream-button-icon" />
+                        Reproducir
+                    </button>
+                </div>
+            ))}
+        </>
+    )
 
     return (
         <Modal header={header} body={body} onClose={onClose} />
