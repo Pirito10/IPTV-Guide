9b023a5950f9d26119b352917a6a20c35ce02633
Pirito10 <aaronriveiro@gmail.com>
2025-04-27 02:52:51 +0200

    refactor ProgramModal

--- frontend/src/components/Modal/ProgramModal.jsx
+++ frontend/src/components/Modal/ProgramModal.jsx
@@ -4,19 +4,24 @@ import '@styles/Modal/ProgramModal.css'
 
 // Componente para mostrar un modal con la información de un programa
 export const ProgramModal = ({ program, logo, onClose }) => {
-    // Creamos la cabecera del modal
+    // Función para poner el logo de respaldo
+    const handleImageError = e => {
+        e.currentTarget.src = FALLBACK_LOGO
+    }
+
+    // Creamos la cabecera del modal, con el logo del canal y el nombre del programa 
     const header = (
         <>
             <img
                 className="modal-logo"
                 src={logo}
-                onError={e => e.currentTarget.src = FALLBACK_LOGO}
+                onError={handleImageError}
             />
             <h2 className="modal-title">{program.title}</h2>
         </>
     )
 
-    // Creamos el cuerpo del modal
+    // Creamos el cuerpo del modal, con la descripción del programa
     const body = (
         <div className="program-modal-description">{program.description}</div>
     )
