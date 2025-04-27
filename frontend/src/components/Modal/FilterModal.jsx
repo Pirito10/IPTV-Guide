a9694878c8799e5174c277a4e63e80888e66e477
Pirito10 <aaronriveiro@gmail.com>
2025-04-27 02:56:49 +0200

    reorder FilterModal

--- frontend/src/components/Modal/FilterModal.jsx
+++ frontend/src/components/Modal/FilterModal.jsx
@@ -3,12 +3,20 @@ import '@styles/Modal/FilterModal.css'
 
 // Componente para mostrar un modal con los grupos para filtrar
 export const FilterModal = ({ groups, selectedGroups, onChange, onClose }) => {
+    // Función para cambiar la selección de un grupo
+    const toggleGroup = (group) => {
+        // Añadimos o eliminamos el grupo de la selección de grupos
+        const updatedSelection = selectedGroups.includes(group) ? selectedGroups.filter(g => g !== group) : [...selectedGroups, group]
+        // Actualizamos los grupos seleccionados con la nueva selección
+        onChange(updatedSelection)
+    }
+
     // Creamos la cabecera del modal
     const header = (
         <h2 className="modal-title">Grupos de canales</h2>
     )
 
-    // Creamos el cuerpo del modal
+    // Creamos el cuerpo del modal, con un selector para cada grupo
     const body = (
         <div className="groups-container">
             {groups.map((group) => (
@@ -23,14 +31,6 @@ export const FilterModal = ({ groups, selectedGroups, onChange, onClose }) => {
             ))}
         </div>
     )
-
-    // Función para cambiar la selección de un grupo
-    const toggleGroup = (group) => {
-        // Añadimos o eliminamos el grupo de la selección de grupos
-        const updatedSelection = selectedGroups.includes(group) ? selectedGroups.filter(g => g !== group) : [...selectedGroups, group]
-        // Actualizamos los grupos seleccionados con la nueva selección
-        onChange(updatedSelection)
-    }
 
     return (
         <Modal header={header} body={body} onClose={onClose} />
