28a20c999417c7d8113cb23727d276c7894d53e4
Pirito10 <aaronriveiro@gmail.com>
2025-05-09 01:20:19 +0200

    split tests into classes

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -10,42 +10,47 @@ def client():
     with app.test_client() as client:
         yield client
 
-@patch('backend.routes.update_m3u')
-def test_channels_no_data(_, client):
-    routes_module.cache.cached_m3u_data = None
-    response = client.get('/api/channels')
+# Tests para la ruta /api/channels
+class TestChannelsRoute:
+    @patch('backend.routes.update_m3u')
+    def test_channels_no_data(self, _, client):
+        routes_module.cache.cached_m3u_data = None
+        response = client.get('/api/channels')
 
-    assert response.status_code == 500
-    assert response.json == {"error": "No channels available"}
+        assert response.status_code == 500
+        assert response.json == {"error": "No channels available"}
 
-@patch('backend.routes.update_m3u')
-def test_channels_success(_, client):
-    routes_module.cache.cached_m3u_data = [{'id': 'test_channel', 'name': 'Test Channel'}]
-    response = client.get('/api/channels')
+    @patch('backend.routes.update_m3u')
+    def test_channels_success(self, _, client):
+        routes_module.cache.cached_m3u_data = [{'id': 'test_channel', 'name': 'Test Channel'}]
+        response = client.get('/api/channels')
 
-    assert response.status_code == 200
-    assert response.json == [{'id': 'test_channel', 'name': 'Test Channel'}]
+        assert response.status_code == 200
+        assert response.json == [{'id': 'test_channel', 'name': 'Test Channel'}]
 
-def test_epg_no_data(client):
-    routes_module.cache.cached_epg_data = None
-    response = client.get('/api/epg')
 
-    assert response.status_code == 500
-    assert response.json == {"error": "No EPG available"}
+# Tests para la ruta /api/epg
+class TestEPGRoute:
+    def test_epg_no_data(self, client):
+        routes_module.cache.cached_epg_data = None
+        response = client.get('/api/epg')
 
-def test_epg_success(client):
-    routes_module.cache.cached_epg_data = [{
-            "title": "Program Title",
-            "description": "Program description",
-            "since": "Start Time",
-            "till": "Stop Time",
-        }]
-    response = client.get('/api/epg')
+        assert response.status_code == 500
+        assert response.json == {"error": "No EPG available"}
 
-    assert response.status_code == 200
-    assert response.json == [{
-            "title": "Program Title",
-            "description": "Program description",
-            "since": "Start Time",
-            "till": "Stop Time",
-        }]
+    def test_epg_success(self, client):
+        routes_module.cache.cached_epg_data = [{
+                "title": "Program Title",
+                "description": "Program description",
+                "since": "Start Time",
+                "till": "Stop Time",
+            }]
+        response = client.get('/api/epg')
+
+        assert response.status_code == 200
+        assert response.json == [{
+                "title": "Program Title",
+                "description": "Program description",
+                "since": "Start Time",
+                "till": "Stop Time",
+            }]
